b55b46b69b6ec5b38147328aa3283fc7295d183d
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-27 18:33:38 -0400

    update

--- clone/pinokio.js
+++ clone/pinokio.js
@@ -9,14 +9,9 @@ module.exports = {
   }],
   run: async (kernel, input) => {
     if (input.path) {
-      // copy the template
-      const folder_path = kernel.path("api", input.path)
-      await fs.promises.cp(path.resolve(__dirname, "template"), folder_path, { recursive: true })
-
-      // clone the remote git repository
       await kernel.exec({
-        message: `git clone ${input.url} app`,
-        path: folder_path
+        message: `git clone ${input.url} ${input.path}`,
+        path: kernel.path("api")
       }, (e) => {
         process.stdout.write(e.raw)
       })
