af728dfeef97472ce586dcc0549449680f84cea0
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-09 04:42:31 -0400

    update

--- docsify/pinokio.js
+++ docsify/pinokio.js
@@ -13,13 +13,6 @@ module.exports = {
       }
     },
     {
-      method: "filepicker.open",
-      params: {
-        title: "Select a folder with README.md",
-        type: "folder"
-      }
-    },
-    {
       method: "input",
       params: {
         title: "Select a folder",
@@ -39,20 +32,29 @@ module.exports = {
       }
     },
     {
-      method: "local.set",
+      // jump to "local" or "git"
+      method: "jump",
       params: {
-        type: "{{input.type}}"
+        id: "{{input.type}}"
       }
     },
     {
-      when: "{{local.type === 'local'}}",
+      id: "local",
+      method: "filepicker.open",
+      params: {
+        title: "Select a folder with README.md",
+        type: "folder"
+      }
+    },
+    {
       method: async (req, ondata, kernel) => {
         await fs.promises.cp(path.resolve(__dirname, "template"), req.cwd, { recursive: true, force: true })
         await fs.promises.cp(req.input.paths[0], path.resolve(req.cwd, 'docs'), { recursive: true, force: true })
       },
-      next: null
+      next: null    // terminate
     },
     {
+      id: "git",
       when: "{{local.type === 'git'}}",
       method: "input",
       params: {
