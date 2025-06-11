33ddb844dd0e82558177b92d46ab175eaf342aab
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-11 08:00:26 -0400

    update

--- nodejs/template/install.js
+++ nodejs/template/install.js
@@ -5,15 +5,6 @@ module.exports = {
       params: {
         path: "app",
         message: [
-          "pnpm install -g @anthropic-ai/claude-code"
-        ],
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        path: "app",
-        message: [
           "pnpm install"
         ],
       }
--- nodejs/template/pinokio.js
+++ nodejs/template/pinokio.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "3.7",
+  version: "4.0",
   icon: "icon.png",
   menu: async (kernel, info) => {
     let installed = info.exists("app/node_modules")
@@ -12,6 +12,11 @@ module.exports = {
         command: cmd
       }
     })
+    cmds.push({
+      icon: "fa-solid fa-terminal",
+      text: "Shell",
+      href: "shell.js"
+    })
     let running = {
       install: info.running("install.js"),
       start: info.running("start.js"),
@@ -21,120 +26,45 @@ module.exports = {
     }
     if (running.install) {
       return [{
-        default: true,
+        id: "shell",
         icon: "fa-solid fa-plug",
         text: "Installing",
         href: "install.js",
       }]
     } else if (installed) {
-      if (running.shell) {
-        return [{
-          icon: "fa-solid fa-terminal",
-          text: "Shell",
-          href: "shell.js",
-        }, {
-          icon: "fa-solid fa-up-right-from-square",
-          text: "Open in...",
-          menu: cmds,
-        }, {
-          icon: "fa-solid fa-power-off",
-          text: "Start",
-          href: "start.js",
-        }, {
-          icon: "fa-solid fa-plug",
-          text: "Update",
-          href: "update.js",
-        }, {
-          icon: "fa-solid fa-plug",
-          text: "Install",
-          href: "install.js",
-        }, {
-          icon: "fa-regular fa-circle-xmark",
-          text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
-          href: "reset.js",
-          confirm: "Are you sure you wish to reset the app?"
-
-        }]
-      } else if (running.start) {
-        let local = info.local("start.js")
-        if (local && local.url) {
-          return [{
-            default: true,
-            icon: "fa-solid fa-rocket",
-            text: "Open Web UI",
-            href: local.url,
-          }, {
-            icon: "fa-solid fa-up-right-from-square",
-            text: "Open in...",
-            menu: cmds,
-          }, {
-            icon: 'fa-solid fa-terminal',
-            text: "Terminal",
-            href: "start.js",
-          }, {
-            icon: "fa-solid fa-terminal",
-            text: "Shell",
-            href: "shell.js",
-          }]
-        } else {
-          return [{
-            default: true,
-            icon: 'fa-solid fa-terminal',
-            text: "Terminal",
-            href: "start.js",
-          }, {
-            icon: "fa-solid fa-up-right-from-square",
-            text: "Open in...",
-            menu: cmds,
-          }, {
-            icon: "fa-solid fa-terminal",
-            text: "Shell",
-            href: "shell.js",
-          }]
-        }
-      } else if (running.update) {
-        return [{
-          default: true,
-          icon: 'fa-solid fa-terminal',
-          text: "Updating",
-          href: "update.js",
-        }]
-      } else if (running.reset) {
-        return [{
-          default: true,
-          icon: 'fa-solid fa-terminal',
-          text: "Resetting",
-          href: "reset.js",
-        }]
-      } else {
-        return [{
-          default: true,
-          icon: "fa-solid fa-terminal",
-          text: "Shell",
-          href: "shell.js",
-        }, {
-          icon: "fa-solid fa-up-right-from-square",
-          text: "Open in...",
-          menu: cmds,
-        }, {
-          icon: "fa-solid fa-power-off",
-          text: "Start",
-          href: "start.js",
-        }, {
-          icon: "fa-solid fa-plug",
-          text: "Update",
-          href: "update.js",
-        }, {
-          icon: "fa-solid fa-plug",
-          text: "Install",
-          href: "install.js",
-        }, {
-          icon: "fa-regular fa-circle-xmark",
-          text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
-          href: "reset.js",
-          confirm: "Are you sure you wish to reset the app?"
-
-        }]
+      let menu = [{
+        icon: "fa-solid fa-up-right-from-square",
+        text: "Open in...",
+        menu: cmds,
+      }, {
+        icon: "fa-solid fa-power-off",
+        text: "Start App",
+        href: "start.js",
+      }, {
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }, {
+        icon: "fa-solid fa-plug",
+        text: "Update",
+        href: "update.js",
+      }, {
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }, {
+        icon: "fa-regular fa-circle-xmark",
+        text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
+        href: "reset.js",
+        confirm: "Are you sure you wish to reset the app?"
+      }]
+      let local = info.local("start.js")
+      if (local && local.url) {
+        menu = [{
+          icon: "fa-solid fa-rocket",
+          text: "Open Web UI",
+          href: local.url,
+        }].concat(menu)
       }
     } else {
       return [{
@@ -142,10 +72,6 @@ module.exports = {
         icon: "fa-solid fa-plug",
         text: "Install",
         href: "install.js",
-      }, {
-        icon: "fa-solid fa-terminal",
-        text: "Shell",
-        href: "shell.js",
       }]
     }
   }
