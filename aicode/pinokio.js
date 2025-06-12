831a7ad4404ce57d95df202ffde2a26c6a4a4ddc
cocktailpeanut <cocktailpeanuts@proton.me>
2025-06-12 16:10:31 -0400

    update

---
+++ aicode/pinokio.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+module.exports = {
+  version: "4.0",
+  type: "extension",
+  title: "claude code",
+  icon: "icon.webp",
+  description: "add claude code to the menu",
+  run: [{
+    method: async (req, ondata, kernel) => {
+      let json = await kernel.require(req.cwd, "pinokio.json")
+      console.log("json", json)
+      json.plugin.menu = json.plugin.menu.concat([{
+        image: "claude.png",
+        text: "Claude Code",
+        href: "claude.json"
+      }, {
+        image: "openai.webp",
+        text: "OpenAI Codex",
+        href: "codex.json"
+      }, {
+        image: "cursor.jpeg",
+        text: "Cursor",
+        run: "cursor ."
+      }, {
+        image: "windsurf.png",
+        text: "Windsurf",
+        run: "windsurf ."
+      })
+      await fs.promises.writeFile(path.resolve(req.cwd, "pinokio.json"), JSON.stringify(json, null, 2))
+    }
+  }]
+}
